9796ca5702c36b1774d70750c0064afd9de4553f
Stephane Gurgenidze <stephane@parity.io>
2025-02-04 23:28:04 +0400

    feat: implement chat server

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
     loop {
-        let (mut socket, _address) = listener.accept().await.unwrap();
+        let (mut socket, address) = listener.accept().await.unwrap();
+
+        let (tx, mut rx) = (tx.clone(), tx.subscribe());
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
             let mut reader = BufReader::new(reader);
+            let mut line = String::new();
 
             loop {
-                let mut line = String::new();
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
-                writer.write_all(line.as_bytes()).await.unwrap();
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), address)).unwrap();
+                        line.clear();
+                    }
+
+                    result = rx.recv() => {
+                        let (message, other_address) = result.unwrap();
+                        if other_address != address {
+                            writer.write_all(message.as_bytes()).await.unwrap();
+                        }
+                    }
+                };
             }
         });
     }
