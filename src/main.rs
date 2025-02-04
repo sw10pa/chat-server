53f4d735557a4f441ef7e23989e2a478a5e7dc7a
Stephane Gurgenidze <stephane@parity.io>
2025-02-04 22:50:35 +0400

    feat: implement echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    loop {
+        let (mut socket, _address) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+
+            loop {
+                let mut line = String::new();
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                writer.write_all(line.as_bytes()).await.unwrap();
+            }
+        });
+    }
 }
